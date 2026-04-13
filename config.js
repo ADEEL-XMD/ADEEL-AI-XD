6e3908fa4c37792c33b9d9d312118a6ad93749ec
DARKSILENCE0 <sitepair4@gmail.com>
2026-04-13 23:27:19 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { getConfig } = require("./lib/configdb");
 
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
